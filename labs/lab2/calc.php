6d5f432b6b7ad25a37d3dd6543ef2fb6b6612b15
NuraAhmedd <144691212+NuraAhmedd@users.noreply.github.com>
2023-09-11 15:43:04 +0300

    Create calc.php

---
+++ labs/lab2/calc.php
@@ -0,0 +1,28 @@
+
+<?php
+
+ function add($x,$y){
+return ($x+$y);
+}
+
+
+ function subtract($x,$y){
+return ($x-$y);
+} 
+
+ function multiply($x,$y){
+return ($x*$y);
+}
+
+ function divide($x,$y){
+return ($x/$y);
+}
+
+echo add(1,2);
+echo subtract(4,1); 
+echo multiply(4,5); 
+echo divide(2,2); 
+
+
+
+?>
